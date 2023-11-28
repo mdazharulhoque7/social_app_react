6a33b9c8b35cce11cd3976569b0f159e37e8e3d1
unknown <azharul.hoque@televersant.com>
2023-11-28 10:44:07 +0600

    initial with react/tailwindcss/shadcn ui and basic scaffold

---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,23 @@
+import { Outlet, Navigate } from "react-router-dom";
+
+Outlet;
+
+const AuthLayout = () => {
+  const isAuthenticated = false;
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section>
+            <Outlet />
+          </section>
+        </>
+      )}
+    </>
+  );
+};
+
+export default AuthLayout;
